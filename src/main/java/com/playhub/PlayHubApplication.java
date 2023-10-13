a843967a112c79b0387a42673f6fd6bdf0636656
kwonminji <minji6313@yahoo.com>
2023-10-13 13:45:28 +0900

    #1.프로젝트 목표 설정

---
+++ src/main/java/com/playhub/PlayHubApplication.java
@@ -0,0 +1,13 @@
+package com.playhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playhub/PlayHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
